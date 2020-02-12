52c941df05237a46700aba4683ebd9591eca5d34
KsRyY <andy200511@126.com>
2020-02-12 15:23:49 +0800

    :art: Coinsistent Indention

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -6,47 +6,47 @@ const commonjs = require('rollup-plugin-commonjs');
 const babel = require('rollup-plugin-babel');
 
 const umd = async function () {
-	const bundle = await rollup({
-		input: './src/index.js',
-		plugins: [
-			resolve(),
-			commonjs(),
-			babel(),
-			terser()
-		]
-	});
+  const bundle = await rollup({
+    input: './src/index.js',
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel(),
+      terser()
+    ]
+  });
 
-	await bundle.write({
-		"file": "./dist/<%= name %>.umd.js",
-		"name": "<%= name %>",
-		"type": "umd",
-		"sourcemap": <%= sourcemap %>
-	})
+  await bundle.write({
+    "file": "./dist/<%= name %>.umd.js",
+    "name": "<%= name %>",
+    "type": "umd",
+    "sourcemap": <%= sourcemap %>
+  })
 }
 
 const esm = async function () {
-	const bundle = await rollup({
-		input: './src/index.js',
-		plugins: [
-			resolve(),
-			commonjs(),
-			builtins(),
-			babel(),
-			terser()
-		]
-	});
+  const bundle = await rollup({
+    input: './src/index.js',
+    plugins: [
+      resolve(),
+      commonjs(),
+      builtins(),
+      babel(),
+      terser()
+    ]
+  });
 
-	await bundle.write({
-		"file": "./dist/<%= name %>.esm.js",
-		"name": "<%= name %>",
-		"type": "esm",
-		"sourcemap": <%= sourcemap %>
-	})
+  await bundle.write({
+    "file": "./dist/<%= name %>.esm.js",
+    "name": "<%= name %>",
+    "type": "esm",
+    "sourcemap": <%= sourcemap %>
+  })
 }
 
 exports.esm = esm;
 exports.umd = umd;
 exports.default = parallel(umd, esm);
 exports.watch = () => {
-	watch('src/*.js', { ignoreInitial: false }, parallel(umd, esm))
+  watch('src/*.js', { ignoreInitial: false }, parallel(umd, esm))
 }
